81d2dc48f8cb11b6db6b91f3d2c848e7ffa11e8a
sergi <a18074@alunos.ipca.pt>
2024-12-09 17:26:56 +0000

    Work Done: - Configuration and adding the project to the firebase database.
    - adding the Splash Activity component.
    
    - adding the necessary libraries to use the Firebase data base.
    
    Notes:
    - ...
    
    * Add items:
    - activity_splash.xml
    - SplashActivity.java
    - google-services.json
    
    * Updated items:
    - AndroidManifest.xml
    - build.gradle.kts
    - libs.versions.toml
    
    * Removed:
    - ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.services)
+
 }
 
 android {
@@ -29,13 +32,23 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.material3)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform(libs.firebase.bom))
+    implementation (libs.androidx.material3)
+
 }
